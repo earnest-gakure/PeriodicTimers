e0bb477078b6bc142b4fb67d0d5ca88bc8d11c58
Earnest <105698709+earnest-gakure@users.noreply.github.com>
2023-08-04 18:27:38 +0300

    Add files via upload

---
+++ 16_bit periodic/main.c
@@ -0,0 +1,33 @@
+//creating delays
+
+#include "TM4C123.h"                    // Device header
+#define BLUE (1U<<2)
+void period_timer (int time );
+
+int main (){
+	SYSCTL->RCGCGPIO |=0x20;
+	GPIOF->DIR       |=BLUE;
+	GPIOF->DEN       |=BLUE;
+	
+	while (1){
+		GPIOF->DATA ^= BLUE;
+		period_timer(60000);
+		
+	}
+}
+void period_timer (int time )
+{
+	int j;
+	SYSCTL->RCGCTIMER |= 0x01 ;
+	TIMER0->CTL       |= 0x00 ; //DISSALBE TIMER
+	TIMER0->CFG       |= 0x04 ; //selecting our timer as 16-bits
+	TIMER0->TAMR      |= 0x02 ;  //selecting periodic timer
+	TIMER0->TAILR     |= 16000000 -1 ; //interval value
+	TIMER0->ICR       |= 0x01 ;     //clear timeout flag
+	TIMER0->CTL       |= 0x01 ;     //enable timer
+	
+	for (j=0;j<time ;j++){
+		while ((TIMER0->RIS &0x1)==0);
+		TIMER0->ICR =0x01;
+	}
+}
